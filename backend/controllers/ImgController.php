3e748f90b0baae3804c0ad090a9f73bc7c784c32
nikolaypavlenko <nnik@bk.ru>
2016-07-12 13:31:09 +0300

    доработка создания, изменения и удаления фото, в том числе файлов с фото на сервере

--- backend/controllers/ImgController.php
+++ backend/controllers/ImgController.php
@@ -69,21 +69,20 @@ class ImgController extends Controller
     public function actionCreate()
     {
         $id = $_GET['id']; //получение id продукта, отправленного с бэкенд/продакт/индекс
-        $images = Img::find()->where(['product_id' => $id])->all();
+        $images = Img::find()->where(['product_id' => $id])->all(); // выбор всех фото по продукту 
         $model = new Img();
-        $model->image = "i"; //присваиваем любое значение антрибуту, иначе не сохраняется в бд. а данные знсим в БД перед сохранением файла, чтобы получить id in table img
+        $foto = time(); // вводим переменную, которую закодирует мд5 при сохранении
 
-            //сохранение загружаемого файла
+        //сохранение загружаемого файла
         if ($model->load(Yii::$app->request->post()) ) {
                 $model->product_id = $id;
-                $model->save();
+                //$model->save();
                 $model->file = UploadedFile::getInstance($model, 'file');
                 if($model->file){
-                    $model->file->saveAs(Yii::getAlias('@frontend/web/images/') . md5($model->id) . '.' . $model->file->extension);
-                    $model->image = '/frontend/web/images/' . md5($model->id) . '.' . $model->file->extension;
+                    $model->file->saveAs(Yii::getAlias('@frontend/web/images/') . md5($foto) . '.' . $model->file->extension);
+                    $model->image = '/frontend/web/images/' . md5($foto) . '.' . $model->file->extension;
                 }
         $model->save(false); //что-бы повторно не валидировалось, иначе присваивается $model->image = "i"
-                //var_dump($model->file); die();
         return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('create', [
@@ -104,16 +103,21 @@ class ImgController extends Controller
     {
         
         $model = $this->findModel($id);
-        $model->image = "i"; //присваиваем любое значение антрибуту, иначе не сохраняется в бд. а данные зaнoсим в БД перед сохранением файла, чтобы получить id in table img
+        $foto = time(); // вводим переменную, которую закодирует мд5 при сохранении
         $images = Img::find()->where(['product_id' => $model->product_id])->all();
-
+        $del = $model->image; // file for delete
+        
         if ($model->load(Yii::$app->request->post()) ) {
                 $model->file = UploadedFile::getInstance($model, 'file');
                 if($model->file){
-                    $model->file->saveAs(Yii::getAlias('@frontend/web/images/') . md5($model->id) . '.' . $model->file->extension);
-                    $model->image = '/frontend/web/images/' . md5($model->id) . '.' . $model->file->extension;
+                    $model->file->saveAs(Yii::getAlias('@frontend/web/images/') . md5($foto) . '.' . $model->file->extension);
+                    $model->image = '/frontend/web/images/' . md5($foto) . '.' . $model->file->extension;
                 }
             $model->save(false);
+            //удаление файла с фото на сайте
+            if($model->save(false)) {
+                    unlink($_SERVER['DOCUMENT_ROOT'] . $del); //указываем полный путь к файлу на сервере
+            }
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
@@ -131,7 +135,10 @@ class ImgController extends Controller
      */
     public function actionDelete($id)
     {
+        $model = $this->findModel($id);        
+        $del = $model->image; // file for delete
         $this->findModel($id)->delete();
+        unlink($_SERVER['DOCUMENT_ROOT'] . $del); //указываем полный путь к файлу на сервере
 
         return $this->redirect(['index']);
     }
